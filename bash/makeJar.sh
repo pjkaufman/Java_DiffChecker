9614aad55380422380746b9e66b4f0709669b419
pjkaufman <pjkaufman@github.com>
2018-04-22 21:16:46 -0700

    update

--- bash/makeJar.sh
+++ bash/makeJar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-javac -cp ".;../jarLibrary/jackson-annotations-2.0.1.jar";".;../jarLibrary/jackson-databind-2.0.1.jar";".;../jarLibrary/jackson-core-2.0.1.jar";".;../jarLibrary/mysql-connector-java-8.0.11-bin.jar" ../db_diff_checker_gui2/*.java
+javac -cp ".;../jarLibrary/jackson-annotations-2.0.1.jar";".;../jarLibrary/jackson-databind-2.0.1.jar";".;../jarLibrary/jackson-core-2.0.1.jar";".;../jarLibrary/mysql-connector-java-8.0.11.jar" ../db_diff_checker_gui2/*.java
 jar cvfm ../run/Db_Diff_Checker.jar ../manifest.mf ../db_diff_checker_gui2/ ../Images ../jarLibrary
-rm -f ../db_diff_checker_gui2/*.class
+rm -fs ../db_diff_checker_gui2/*.class
+cp ../jarLibrary/*.jar ../run/lib/
 clear && clear
